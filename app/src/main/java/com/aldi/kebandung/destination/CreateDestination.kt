e379874634078d778ad2b01372a23cc812a432bf
ganiaaldi <mrgad98@gmail.com>
2020-11-20 07:10:10 +0700

    implement uploadphoto btn

--- app/src/main/java/com/aldi/kebandung/destination/CreateDestination.kt
+++ app/src/main/java/com/aldi/kebandung/destination/CreateDestination.kt
@@ -1,9 +1,14 @@
 package com.aldi.kebandung.destination
 
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.ProgressDialog
 import android.app.TimePickerDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -19,6 +24,8 @@ import com.aldi.kebandung.R
 import kotlinx.android.synthetic.main.fragment_create_destination.*
 import android.widget.TimePicker
 import androidx.activity.addCallback
+import androidx.core.app.ActivityCompat.startActivityForResult
+import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.navigateUp
@@ -34,6 +41,12 @@ import java.util.*
 
 
 class CreateDestination : Fragment(){
+    companion object {
+        //image pick code
+        private val IMAGE_PICK_CODE = 1000;
+        //Permission code
+        private val PERMISSION_CODE = 1001;
+    }
 
     var selectedSpinner : String? = null
     var selectedSpinnerr : String? = null
@@ -56,6 +69,7 @@ class CreateDestination : Fragment(){
         createDestination()
         OnClickTime()
         buttonPressed()
+        uploadImage()
         //fan()
     }
 
@@ -380,12 +394,70 @@ class CreateDestination : Fragment(){
                 })
         }
 
-          //  .addBodyParameter("email",inputEmail.text.toString())
+
+
+        //  .addBodyParameter("email",inputEmail.text.toString())
           //  .addBodyParameter("username",inputUsername.text.toString())
          //   .addBodyParameter("nama_lengkap",inputUsernameFull.text.toString())
           //  .addBodyParameter("kata_sandi",inputPassword.text.toString())
 
     }
 
+    private fun uploadImage() {
+        uploadPhoto.setOnClickListener {
+            uploadPhoto.visibility = View.INVISIBLE
+            uploadPhotoDone.visibility = View.VISIBLE
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(context!!,Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_DENIED){
+                    //permission denied
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
+                    //show popup to request runtime permission
+                    requestPermissions(permissions, PERMISSION_CODE);
+                }
+                else{
+                    //permission already granted
+                    pickImageFromGallery();
+                }
+            }
+            else{
+                //system OS is < Marshmallow
+                pickImageFromGallery();
+            }
+        }
+    }
 
+
+private fun pickImageFromGallery() {
+    //Intent to pick image
+    val intent = Intent(Intent.ACTION_PICK)
+    intent.type = "image/*"
+    startActivityForResult(intent, IMAGE_PICK_CODE)
 }
+
+
+//handle requested permission result
+override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    when(requestCode){
+        PERMISSION_CODE -> {
+            if (grantResults.size >0 && grantResults[0] ==
+                PackageManager.PERMISSION_GRANTED){
+                //permission from popup granted
+                pickImageFromGallery()
+            }
+            else{
+                //permission from popup denied
+                Toast.makeText(context, "Permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
+
+//handle result of picked image
+override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+        uploadPhotoDone.setImageURI(data?.data)
+    }
+}
+}
+
